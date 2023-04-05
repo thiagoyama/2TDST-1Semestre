05ce8b8deabbff8f0e90842fe6b470c49b162f2f
Logon PF Fiap <logonpflocal@fiap.com.br>
2023-04-05 20:55:09 -0300

    Relacionamentos

--- 05-Jpa-Padroes/src/main/java/br/com/fiap/jpa/dao/LivroDaoImpl.java
+++ 05-Jpa-Padroes/src/main/java/br/com/fiap/jpa/dao/LivroDaoImpl.java
@@ -3,7 +3,9 @@ package br.com.fiap.jpa.dao;
 import javax.persistence.EntityManager;
 import br.com.fiap.jpa.entity.Livro;
 
-public class LivroDaoImpl extends GenericDaoImpl<Livro, Integer> implements LivroDao {
+public class LivroDaoImpl 
+			extends GenericDaoImpl<Livro, Integer> 
+										implements LivroDao {
 
 	public LivroDaoImpl(EntityManager em) {
 		super(em);
--- 05-Jpa-Padroes/src/main/java/br/com/fiap/jpa/view/View.java
+++ 05-Jpa-Padroes/src/main/java/br/com/fiap/jpa/view/View.java
@@ -1,9 +1,7 @@
 package br.com.fiap.jpa.view;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,41 +11,60 @@ import br.com.fiap.jpa.dao.LivroDaoImpl;
 import br.com.fiap.jpa.entity.Genero;
 import br.com.fiap.jpa.entity.Livro;
 import br.com.fiap.jpa.exception.CommitException;
+import br.com.fiap.jpa.exception.EntidadeNaoEcontradaException;
 import br.com.fiap.jpa.sigleton.EntityManagerFactorySingleton;
 
 public class View {
 
 	public static void main(String[] args) {
-		//Obter uma fabrica de Entity Manager
+		// Obter uma fabrica de Entity Manager
 		EntityManagerFactory fabrica = EntityManagerFactorySingleton.getInstance();
 		EntityManager em = fabrica.createEntityManager();
-		
-		//Instanciar o LivroDao
+
+		// Instanciar o LivroDao
 		LivroDao dao = new LivroDaoImpl(em);
-		
-		List<String> lista = new ArrayList<String>();
-		
-		//Instaciar um Livro (criar o construtor)
-		Livro livro = new Livro("João e Maria", 
-				Genero.AVENTURA, 390, new GregorianCalendar(1812, Calendar.DECEMBER, 20));
-		
-		//Cadastrar um Livro
+//		GenericDao<Livro, Integer> dao = 
+//				new GenericDaoImpl<Livro, Integer>(em) {};
+
+		// Instaciar um Livro (criar o construtor)
+		Livro livro = new Livro("Joao e Maria", Genero.AVENTURA, 390,
+				new GregorianCalendar(1812, Calendar.DECEMBER, 20));
+
+		// Cadastrar um Livro
 		try {
 			dao.salvar(livro);
 			dao.commit();
 			System.out.println("Livro cadastrado!");
-		}catch(CommitException e) {
+		} catch (CommitException e) {
 			System.out.println(e.getMessage());
 		}
-		
-		//Pesquisar um livro pelo id
-		
-		
-		//Atualizar um livro
-		
-		
-		//Remover um livro
-		
-		
+
+		// Pesquisar um livro pelo id
+		try {
+			Livro busca = dao.buscar(100);
+			System.out.println(busca.getTitulo());
+		} catch (EntidadeNaoEcontradaException e) {
+			System.err.println(e.getMessage());
+		}
+
+		// Atualizar um livro
+		try {
+			livro.setCodigo(1);
+			livro.setTitulo("Clean Code");
+			dao.salvar(livro);
+			dao.commit();
+			System.out.println("Livro atualizado!");
+		} catch (CommitException e) {
+			System.err.println(e.getMessage());
+		}
+
+		// Remover um livro
+		try {
+			dao.deletar(6);
+			dao.commit();
+			System.out.println("Livro removido!");
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
