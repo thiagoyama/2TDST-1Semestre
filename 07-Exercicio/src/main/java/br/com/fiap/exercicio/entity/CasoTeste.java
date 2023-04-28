7d8212457d817a89def2166d2c3a4c85d0fd1902
Thiago Yamamoto <thiagoyama@gmail.com>
2023-04-27 22:51:46 -0300

    Exercicio

---
+++ 07-Exercicio/src/main/java/br/com/fiap/exercicio/entity/CasoTeste.java
@@ -0,0 +1,88 @@
+package br.com.fiap.exercicio.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TAB_CASO_TESTE")
+@SequenceGenerator(name="caso", sequenceName = "SQ_TAB_CASO_TESTE", allocationSize = 1)
+public class CasoTeste {
+
+	@Id
+	@Column(name="cod_caso_teste")
+	@GeneratedValue(generator = "caso", strategy = GenerationType.SEQUENCE)
+	private Integer codigo;
+	
+	@Column(name="nom_caso_teste")
+	private String nome;
+	
+	@Column(name="des_caso_teste")
+	private String descricao;
+	
+	//Relação N:1 
+	@ManyToOne
+	@JoinColumn(name="cod_sistema", nullable = false)
+	private Sistema sistema;
+	
+	//Relação 1:N
+	@OneToMany(mappedBy = "caso", cascade = CascadeType.ALL)
+	private List<ItemTeste> itensTeste = new ArrayList<ItemTeste>();
+	
+	public void addItemTeste(ItemTeste item) {
+		itensTeste.add(item);
+		item.setCaso(this);
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Sistema getSistema() {
+		return sistema;
+	}
+
+	public void setSistema(Sistema sistema) {
+		this.sistema = sistema;
+	}
+
+	public List<ItemTeste> getItensTeste() {
+		return itensTeste;
+	}
+
+	public void setItensTeste(List<ItemTeste> itensTeste) {
+		this.itensTeste = itensTeste;
+	}
+	
+}
---
+++ 07-Exercicio/src/main/java/br/com/fiap/exercicio/main/Cadastro.java
@@ -0,0 +1,31 @@
+package br.com.fiap.exercicio.main;
+
+public class Cadastro {
+
+	//Cadastrar todas as entidades utilizando o cascade
+	public static void main(String[] args) {
+		
+		//Fabrica e Entity Manager
+		
+		//Instanciar o SistemaDao
+		
+		//Instanciar o sistema
+		
+		//Instanciar 2 casos de teste
+		
+		//Adicionar os casos de teste no sistema
+		
+		//Instanciar 2 itens de teste
+		
+		//Adicionar os itens de teste no caso de teste
+		
+		//Instanciar 2 usuários
+		
+		//Criar uma lista de Usuários e adicionar os usuários
+		
+		//Setar a lista de usuário nos itens de teste
+		
+		//Cadastrar o Sistema
+		
+	}
+}
